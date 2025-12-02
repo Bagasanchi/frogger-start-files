853651ccf909e041a707dbb1249b1c362019bee5
Bagasanchi <bagaezwin@gmail.com>
2025-12-02 14:03:00 +0800

    Player Animation Complete

--- player.gd
+++ player.gd
@@ -16,5 +16,9 @@ func _physics_process(_delta: float) -> void:
 func animation():
 	if direction:
 		$AnimatedSprite2D.flip_h = direction.x > 0
+		if direction.x != 0:
+			$AnimatedSprite2D.animation = "left"
+		else:
+			$AnimatedSprite2D.animation = "up" if direction.y < 0 else "down"
 	else:
 		$AnimatedSprite2D.frame = 0
