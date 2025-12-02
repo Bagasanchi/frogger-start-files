214034670c0abfa14355db434ff749755a1ec601
Bagasanchi <bagaezwin@gmail.com>
2025-12-02 11:06:58 +0800

    Player Animations

--- map.gd
+++ map.gd
@@ -1,7 +1,1 @@
-extends Sprite2D
-
-var direction: Vector2 = Vector2(1, 1)
-var speed: int = 5
-
-func _process(delta: float) -> void:
-	$".".scale += direction * speed * delta
+extends Node2D
--- player.gd
+++ player.gd
@@ -1,7 +1,20 @@
-extends Node2D
+extends CharacterBody2D
 
-var direction: Vector2 = Vector2(1,0)
-var speed: int = 10
+var direction: Vector2 = Vector2(1,1)
+var speed: int = 100
 
-func _physics_process(delta: float) -> void:
-	position += direction * speed
+func _physics_process(_delta: float) -> void:
+	direction = Input.get_vector("left", "right", "up", "down")
+
+	velocity = direction * speed
+	animation()
+	move_and_slide()
+
+	if Input.is_action_just_pressed("space"):
+		print("something")
+
+func animation():
+	if direction:
+		$AnimatedSprite2D.flip_h = direction.x > 0
+	else:
+		$AnimatedSprite2D.frame = 0
